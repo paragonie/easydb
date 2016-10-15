5d8c776d5eeee991f747d8277dfd7bb141f1a0ab
SignpostMarv <me@signpostmarv.name>
2016-10-15 14:21:29 +0100

    adding tests for most uses of EasyDB::is1DArray()

--- tests/Is1DArrayTest.php
+++ tests/Is1DArrayTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace ParagonIE\EasyDB\Tests;
 
+use InvalidArgumentException;
 use ParagonIE\EasyDB\EasyDB;
 use ParagonIE\EasyDB\Factory;
 
@@ -22,5 +23,113 @@ class Is1DArrayTest
         $this->assertTrue($db->is1DArray([1]));
         $this->assertFalse($db->is1DArray([[1]]));
         $this->assertFalse($db->is1DArray([[1],[2]]));
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testColumnThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->column('SELECT "column"', [[1]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testDeleteThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->delete('irrelevant_but_valid_tablename', [[1]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testEscapeValueSetThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->escapeValueSet([[1]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testInsertThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->insert('irrelevant_but_valid_tablename', [[1]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testInsertManyThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->insertMany('irrelevant_but_valid_tablename', [[[2]]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testSafeQueryThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->safeQuery('SELECT ?', [[1]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testSingleThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->single('SELECT "column"', [[1]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testUpdateArgChangesThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->update('irrelevant_but_valid_tablename', [[1]], ['1=1']);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    * @depends testIs1DArray
+    */
+    public function testUpdateArgConditionsThrowsException($dsn, $username=null, $password=null, $options = array())
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->expectException(InvalidArgumentException::class);
+        $db->update('irrelevant_but_valid_tablename', ['1=1'], [[1]]);
     }
 }
