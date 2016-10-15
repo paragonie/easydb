7c8dacd286be4fc6057e7453f1bdc961845888f2
SignpostMarv <me@signpostmarv.name>
2016-10-15 15:56:23 +0100

    testing EasyDB::inTransaction()

---
+++ tests/InTransactionTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace ParagonIE\EasyDB\Tests;
+
+use ParagonIE\EasyDB\EasyDB;
+use ParagonIE\EasyDB\Factory;
+use PDO;
+use PDOException;
+
+class InTransactionTest
+    extends
+        EasyDBTest
+{
+
+    /**
+    * @dataProvider GoodFactoryCreateArgumentProvider
+    */
+    public function testInTransaction($dsn, $username=null, $password=null, $options = array(), $expectedDriver)
+    {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $this->assertInstanceOf(EasyDB::class, $db);
+        $this->assertFalse($db->inTransaction());
+        $db->beginTransaction();
+        $this->assertTrue($db->inTransaction());
+        $db->commit();
+        $this->assertFalse($db->inTransaction());
+        $db->beginTransaction();
+        $this->assertTrue($db->inTransaction());
+        $db->rollback();
+        $this->assertFalse($db->inTransaction());
+    }
+}
