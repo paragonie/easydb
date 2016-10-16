99abab2f881aa593c47fe27c7d9b74f9db5a5b96
SignpostMarv <me@signpostmarv.name>
2016-10-16 15:08:14 +0100

    more tests for ParagonIE\EasyDB\EasyDB::insertMany()

--- tests/InsertManyTest.php
+++ tests/InsertManyTest.php
@@ -21,4 +21,57 @@ class InsertManyTest
 
         $this->assertFalse($db->insertMany('irrelevant_but_valid_tablename', []));
     }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgument2EasyDBProvider
+    */
+    public function testInsertManyThrowsException(callable $cb)
+    {
+        $db = $this->EasyDBExpectedFromCallable($cb);
+        $this->expectException(InvalidArgumentException::class);
+        $db->insertMany('irrelevant_but_valid_tablename', [[], [1]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgument2EasyDBProvider
+    */
+    public function testInsertManyArgTableThrowsException(callable $cb)
+    {
+        $db = $this->EasyDBExpectedFromCallable($cb);
+        $this->expectException(InvalidArgumentException::class);
+        $db->insertMany('', [['foo' => 1], ['foo' => 2]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgument2EasyDBProvider
+    */
+    public function testInsertManyArgMapKeysThrowsException(callable $cb)
+    {
+        $db = $this->EasyDBExpectedFromCallable($cb);
+        $this->expectException(InvalidArgumentException::class);
+        $db->insertMany('irrelevant_but_valid_tablename', [['1foo' => 1]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgument2EasyDBProvider
+    */
+    public function testInsertManyArgMapIs1DArrayThrowsException(callable $cb)
+    {
+        $db = $this->EasyDBExpectedFromCallable($cb);
+        $this->expectException(InvalidArgumentException::class);
+        $db->insertMany('irrelevant_but_valid_tablename', [['foo' => [1]]]);
+    }
+
+    /**
+    * @dataProvider GoodFactoryCreateArgument2EasyDBProvider
+    */
+    public function testInsertMany(callable $cb)
+    {
+        $db = $this->EasyDBExpectedFromCallable($cb);
+        $db->insertMany('irrelevant_but_valid_tablename', [['foo' => '1'], ['foo' => '2']]);
+        $this->assertEquals(
+            $db->single('SELECT COUNT(*) FROM irrelevant_but_valid_tablename'),
+            2
+        );
+    }
 }
