11ed97aa52f21593022ac266fe1f7c1d49414517
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-03 20:07:00 -0400

    Pretty code.

--- src/Exception/ConstructorFailed.php
+++ src/Exception/ConstructorFailed.php
@@ -1,11 +1,24 @@
 <?php
-namespace ParagonIE\EasyDB\Exception;
-
+declare(strict_types=1);
 /**
- * ConstructorFailed.
+ * Paragon Initiative Enterprises.
+ *
+ * @author  Scott Arciszewski   <scott@paragonie.com>.
+ * @author  EasyDB Contributors <https://github.com/paragonie/easydb/graphs/contributors>
+ *
+ * @link    <https://github.com/paragonie/easydb> Github Repository.
+ * @license <https://github.com/paragonie/easydb/blob/master/LICENSE> MIT License.
  *
  * @package ParagonIE\EasyDB
  */
-class ConstructorFailed extends \RuntimeException implements ExceptionInterface
+
+namespace ParagonIE\EasyDB\Exception;
+
+use RuntimeException;
+
+/**
+ * ConstructorFailed.
+ */
+class ConstructorFailed extends RuntimeException implements ExceptionInterface
 {
 }
