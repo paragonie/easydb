a3e3694a1d8e5ebd479f58c271ef8933240d29a1
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-03 23:37:33 -0400

    Add docblock header.

--- src/EasyStatement.php
+++ src/EasyStatement.php
@@ -1,4 +1,16 @@
 <?php
+declare(strict_types=1);
+/**
+ * Paragon Initiative Enterprises.
+ *
+ * @author  Scott Arciszewski   <scott@paragonie.com>.
+ * @author  EasyDB Contributors <https://github.com/paragonie/easydb/graphs/contributors>
+ *
+ * @link    <https://github.com/paragonie/easydb> Github Repository.
+ * @license <https://github.com/paragonie/easydb/blob/master/LICENSE> MIT License.
+ *
+ * @package ParagonIE\EasyDB
+ */
 
 namespace ParagonIE\EasyDB;
 
