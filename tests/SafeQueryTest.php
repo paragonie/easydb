dfed45ad8ab84fb75ab034c04af67423a0bc28c4
SignpostMarv <me@signpostmarv.name>
2018-10-31 18:37:39 +0000

    re paragonie/easydb#85, adding additional argument to give a slightly enhanced error message on the assumption that a > 1d array being passed to `EasyDB::safeQuery()` was done in error via a method like `EasyDB::run($q, $args)` instead of `EasyDB::run($q, ...$args)`

--- tests/SafeQueryTest.php
+++ tests/SafeQueryTest.php
@@ -4,11 +4,47 @@ declare(strict_types=1);
 namespace ParagonIE\EasyDB\Tests;
 
 use ParagonIE\EasyDB\EasyDB;
+use ParagonIE\EasyDB\Factory;
 
 class SafeQueryTest extends RunTest
 {
     protected function getResultForMethod(EasyDB $db, $statement, $offset, $params)
     {
         return $db->safeQuery($statement, $params);
+    }
+
+    /**
+     * @dataProvider goodFactoryCreateArgumentProvider
+     * @param $dsn
+     * @param null $username
+     * @param null $password
+     * @param array $options
+     */
+    public function testSafeQueryCalledWithVariadicParamsThrowsException(
+        $expectedDriver,
+        $dsn,
+        $username = null,
+        $password = null,
+        $options = []
+    ) {
+        $db = Factory::create($dsn, $username, $password, $options);
+        $args = [1, 2, 3, 4];
+        $results = $db->run('SELECT ? AS foo, ? AS bar UNION SELECT ? AS foo, ? AS bar', ...$args);
+        $this->assertInternalType('array', $results);
+
+        $expectedResult = [['foo' => 1, 'bar' => 2], ['foo' => 3, 'bar' => 4]];
+
+        foreach ($results as $i => $result) {
+            $this->assertInternalType('array', $result);
+            $this->assertEquals(array_diff_assoc($result, $expectedResult[$i]), []);
+        }
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Only one-dimensional arrays are allowed, please use ' .
+            EasyDB::class .
+            '::safeQuery()'
+        );
+        $db->run('SELECT ? AS foo, ? AS bar UNION SELECT ? AS foo, ? AS bar', $args);
     }
 }
