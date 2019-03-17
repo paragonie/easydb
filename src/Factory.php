c62a51ba825344c7ceb9cab3905393e006f6f4c7
Paragon Initiative Enterprises <security@paragonie.com>
2019-03-17 00:35:50 -0400

    Allow the original exception to be extracted from ConstructorFailed.
    Fixes #107. Alternative to #108.

--- src/Factory.php
+++ src/Factory.php
@@ -84,21 +84,21 @@ abstract class Factory
             $pdo = new \PDO($dsn, $username, $password, $options);
         } catch (\PDOException $e) {
             if (\strpos((string) $e->getMessage(), 'could not find driver') !== false) {
-                throw new Issues\ConstructorFailed(
+                throw (new Issues\ConstructorFailed(
                     'Could not create a PDO connection. Is the driver installed/enabled?'
-                );
+                ))->setRealException($e);
             }
             
             if (\strpos((string) $e->getMessage(), 'unknown database') !== false) {
-                throw new Issues\ConstructorFailed(
+                throw (new Issues\ConstructorFailed(
                     'Could not create a PDO connection. Check that your database exists.'
-                );
+                ))->setRealException($e);
             }
             
             // Don't leak credentials directly if we can.
-            throw new Issues\ConstructorFailed(
+            throw (new Issues\ConstructorFailed(
                 'Could not create a PDO connection. Please check your username and password.'
-            );
+            ))->setRealException($e);
         }
 
         if (!empty($post_query)) {
