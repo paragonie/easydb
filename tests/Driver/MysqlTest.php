43971fc703e9e23642c9d658c986951e511d6630
Paragon Initiative Enterprises <security@paragonie.com>
2025-09-24 23:48:17 -0400

    Create reproduction of issue #150

--- tests/Driver/MysqlTest.php
+++ tests/Driver/MysqlTest.php
@@ -29,4 +29,26 @@ class MysqlTest extends BaseTest
     {
         return \getenv('MYSQL_PASS') ?: '';
     }
+
+    public function testIssue150(): void
+    {
+        try {
+            $this->db->exec('CREATE TABLE bit1_test (id INT PRIMARY KEY, val BIT(1))');
+        } catch (\Exception $ex) {
+            $this->markTestSkipped('Cannot perform issue 150 test');
+        }
+        $this->db->insert('bit1_test', ['id' => 1, 'val' => true]);
+        $this->db->insert('bit1_test', ['id' => 2, 'val' => false]);
+        $this->db->insert('bit1_test', ['id' => 3, 'val' => true]);
+
+        $this->assertSame(
+            2,
+            $this->db->single('SELECT COUNT(*) FROM bit1_test WHERE val = ?', [true])
+        );
+
+        $this->assertSame(
+            1,
+            $this->db->single('SELECT COUNT(*) FROM bit1_test WHERE val = ?', [false])
+        );
+    }
 }
