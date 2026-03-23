d992546fa9bd032acf7b86c9daee14e23fdda735
Paragon Initiative Enterprises <security@paragonie.com>
2026-03-23 03:37:00 -0400

    Expand test coverage

--- tests/TrivialMutantTest.php
+++ tests/TrivialMutantTest.php
@@ -2,15 +2,26 @@
 declare(strict_types=1);
 namespace ParagonIE\EasyDB\Tests;
 
+use ParagonIE\EasyDB\EasyDB;
 use ParagonIE\EasyDB\EasyStatement;
 use ParagonIE\EasyDB\Exception\EasyDBException;
+use ParagonIE\EasyDB\Exception\ConstructorFailed;
+use ParagonIE\EasyDB\Exception\MustBeNonEmpty;
+use ParagonIE\EasyDB\Exception\MustBeOneDimensionalArray;
+use ParagonIE\EasyDB\Factory;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use TypeError;
 
 /**
  * Tests that kill trivial escaped mutants surfaced by mutation testing.
  */
 #[CoversClass(EasyStatement::class)]
+#[CoversClass(MustBeNonEmpty::class)]
+#[CoversClass(MustBeOneDimensionalArray::class)]
+#[CoversClass(ConstructorFailed::class)]
+#[CoversClass(Factory::class)]
+#[CoversClass(EasyDB::class)]
 class TrivialMutantTest extends TestCase
 {
     public function testExceptionConstructor(): void
@@ -19,6 +30,16 @@ class TrivialMutantTest extends TestCase
         $this->assertSame('', $ex->getMessage());
         $this->assertSame(0, $ex->getCode());
         $this->assertNull($ex->getPrevious());
+
+        $ex2 = new MustBeNonEmpty();
+        $this->assertSame('', $ex2->getMessage());
+        $this->assertSame(0, $ex2->getCode());
+        $this->assertNull($ex2->getPrevious());
+
+        $ex3 = new MustBeOneDimensionalArray();
+        $this->assertSame('', $ex3->getMessage());
+        $this->assertSame(0, $ex3->getCode());
+        $this->assertNull($ex3->getPrevious());
     }
 
     public function testEasyStatementPublic(): void
@@ -42,5 +63,56 @@ class TrivialMutantTest extends TestCase
         $st4->orWithString('baz = ?', 'qux');
         $this->assertSame('policy IS NOT NULL AND foo = ? OR baz = ?', $st4->sql());
         $this->assertSame(['bar', 'qux'], $st4->values());
+    }
+
+    public function testFactoryFromArrayInvalidArgIndices(): void
+    {
+        $this->expectException(TypeError::class);
+        $this->expectExceptionMessageMatches('/Argument #\d+ \(\$\w+\) must be of type string/');
+        // $dsn requires string, we pass array
+        Factory::fromArray([['array']]);
+    }
+
+    public function testFactoryMissingDriver(): void
+    {
+        $this->expectException(ConstructorFailed::class);
+        $this->expectExceptionMessage('Could not create a PDO connection. Is the driver installed/enabled?');
+        Factory::create('fake_driver_that_does_not_exist:host=localhost');
+    }
+
+    public function testFactoryBadCredentials(): void
+    {
+        if (!extension_loaded('pdo_mysql')) {
+            $this->markTestSkipped('pdo_mysql is not available');
+        }
+        $this->expectException(ConstructorFailed::class);
+        $this->expectExceptionMessage('Could not create a PDO connection. Please check your username and password.');
+        Factory::create(
+            'mysql:host=127.0.0.1;dbname=database_that_absolutely_does_not_exist',
+            'baduser',
+            'badpass'
+        );
+    }
+
+    public function testEasyDBEscapeValueSetEmpty(): void
+    {
+        $db = Factory::create('sqlite::memory:');
+        $this->assertSame('(SELECT 1 WHERE FALSE)', $db->escapeValueSet([]));
+    }
+
+    public function testEasyDBColOffset(): void
+    {
+        $db = Factory::create('sqlite::memory:');
+        $db->query("CREATE TABLE test_col (a int, b int, c int)");
+        $db->insert('test_col', ['a' => 1, 'b' => 2, 'c' => 3]);
+        $db->insert('test_col', ['a' => 4, 'b' => 5, 'c' => 6]);
+
+        // test offset 1 -> b
+        $result = $db->col("SELECT * FROM test_col", 1);
+        $this->assertSame([2, 5], $result);
+
+        // test column with offset 2 -> c
+        $result2 = $db->column("SELECT * FROM test_col", [], 2);
+        $this->assertSame([3, 6], $result2);
     }
 }
